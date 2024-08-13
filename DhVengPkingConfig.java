b5588c582a21db4f6b3b1db0dc571d4e27c13277
bighitss <smartcake55@yahoo.com>
2024-08-13 04:33:00 -0700

    Create DhVengPkingConfig.java

---
+++ DhVengPkingConfig.java
@@ -0,0 +1,9 @@
+package com.yourname.dhvengpking;
+
+import net.runelite.client.config.Config;
+import net.runelite.client.config.ConfigGroup;
+
+@ConfigGroup("dhvengpking")
+public interface DhVengPkingConfig extends Config
+{
+}
